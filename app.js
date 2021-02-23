96316f8e3bed8d2f8c26f69308b9ccc889d7dfa1
Jessica <jessica@Jessicas-MacBook-Pro.local>
2021-02-23 15:59:42 -0800

    create html form & get method in js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("Let's get this party started!");
+
+$("form").on("submit", function (e) {
+    e.preventDefault();
+
+    getData();
+});
+
+
+async function getData() {
+    let searchTerm = $("input").val();
+    console.log(searchTerm)
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&q=${searchTerm}`);
+    console.log("response = ", response)
+    return response;
+}
+
+// api.giphy.com/v1/gifs/search
+
+// api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
