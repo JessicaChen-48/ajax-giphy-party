eeae5299342af9a2ce5bd0f801c344b3c0b69b98
Jessica <jessica@Jessicas-MacBook-Pro.local>
2021-02-23 17:19:35 -0800

    refactor code

--- app.js
+++ app.js
@@ -2,37 +2,29 @@
 
 console.log("Let's get this party started!");
 
-let $gifContainer = $('<div>').attr('id','container');
-$('body').append($gifContainer);
 $("form").on("submit", function (e) {
     e.preventDefault();
     addImage();
 });
 
-function emptyContainer() {
-    $gifContainer.empty();
-}
-
-$('#remove-images').on('click', emptyContainer);
-
 async function addImage() {
-    let results = await getData();
-    let img = results[0].images.original.url;
-    let $new_div = $('<div>');
-    $gifContainer.append($new_div);
-    let $img = $('<img>').attr('src', img);
-    $new_div.append($img);
-        
+    let url = await getData();
+
+    let newImg = $('<img>').attr('src', url);
+    $("#gif-container").append(newImg)
 }
 
 async function getData() {
     let searchTerm = $("input").val();
-    console.log(searchTerm)
     let response = await axios.get(`http://api.giphy.com/v1/gifs/search?api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&q=${searchTerm}`);
-    console.log("response = ", response)
-    return response.data.data;
+
+    let results = response.data.data;
+    let randomN = Math.floor(Math.random() * results.length);
+    return results[randomN].images.original.url;
 }
 
-// api.giphy.com/v1/gifs/search
+function emptyContainer() {
+    $("#gif-container").empty();
+}
 
-// api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+$('#remove-images').on('click', emptyContainer);
